0e3d1eaeaa9fd6e8cae1b3eabe2bef6629741499
dylnbk <dyln.bk@gmail.com>
2022-05-26 23:37:39 +0100

    Date & time fix

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,21 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("media");
     eleventyConfig.addPassthroughCopy("admin");
-}
+
+    const {
+        DateTime
+      } = require("luxon");
+    
+        eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+          return DateTime.fromJSDate(dateObj, {
+            zone: 'utc'
+          }).toFormat('yy-MM-dd');
+        });
+    
+        eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {
+          zone: 'utc'
+        }).toFormat("dd-MM-yy");
+      });
+    
+};
